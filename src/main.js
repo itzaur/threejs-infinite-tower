dfe8cc022a52455d13b915a81245c5ebf6c8b519
itzaur <preneur85@gmail.com>
2023-11-14 14:45:56 +0300

    feat: add tower

--- src/main.js
+++ src/main.js
@@ -10,6 +10,19 @@ export default class Experience {
     this.width = window.innerWidth;
     this.height = window.innerHeight;
 
+    // Parameters
+    this.parameters = {
+      time: 0,
+      duration: 3,
+      fps: 60,
+      playhead: 0,
+      activeRows: 4,
+    };
+
+    this.parameters.step = 1 / (this.parameters.fps * this.parameters.duration);
+
+    this.animation = [];
+
     this.resize = () => this.onResize();
   }
 
@@ -31,6 +44,7 @@ export default class Experience {
 
   createScene() {
     this.scene = new THREE.Scene();
+    this.scene.position.y = 3;
   }
 
   createCamera() {
@@ -40,7 +54,7 @@ export default class Experience {
       0.1,
       1000
     );
-    this.camera.position.set(0, 0, 2);
+    this.camera.position.set(0, 4, 8);
   }
 
   createRenderer() {
@@ -52,20 +66,86 @@ export default class Experience {
   }
 
   createMesh() {
-    this.geometry = new THREE.BoxGeometry(1, 1, 1);
-    this.material = new THREE.MeshPhysicalMaterial({ color: 0x0060ff });
-    this.mesh = new THREE.Mesh(this.geometry, this.material);
-    this.scene.add(this.mesh);
+    const count = 12;
+    const radius = 3;
+    const rows = 10;
+    let angle = null;
+    let width = null;
+    let random = Array(this.parameters.activeRows)
+      .fill()
+      .map((el) => Array(el));
+
+    this.group = new THREE.Group();
+    this.scene.add(this.group);
+
+    for (let i = 0; i < count; i++) {
+      for (let j = 0; j < this.parameters.activeRows; j++) {
+        random[j][i] = Math.random() < 0.5 ? 0 : 1;
+      }
+    }
+
+    for (let i = 0; i < rows; i++) {
+      for (let j = 0; j < count; j++) {
+        angle =
+          (Math.PI * 2 * j) / count +
+          ((i % this.parameters.activeRows) * Math.PI) / count;
+
+        width = radius * 2 * Math.sin(Math.PI / count);
+
+        this.material = new THREE.MeshLambertMaterial({
+          color: random[j % this.parameters.activeRows][i]
+            ? 0xffffff
+            : 0xff0000,
+        });
+        this.geometry = new THREE.BoxGeometry(width, 1, 1);
+
+        this.mesh = new THREE.Mesh(this.geometry, this.material);
+        this.duplicateMesh = new THREE.Mesh(this.geometry, this.material);
+
+        this.mesh.position.set(
+          (radius + 0.39) * Math.sin(angle),
+          0,
+          (radius + 0.39) * Math.cos(angle)
+        );
+        this.duplicateMesh.position.set(
+          (radius + 0.39) * Math.sin(angle),
+          0,
+          (radius + 0.39) * Math.cos(angle)
+        );
+
+        this.mesh.position.setY(-i);
+        this.duplicateMesh.position.setY(-i);
+
+        this.mesh.rotation.y = angle;
+        this.duplicateMesh.rotation.y = angle;
+
+        this.group.add(this.mesh, this.duplicateMesh);
+
+        this.duplicateMesh.visible = false;
+
+        this.animation.push({
+          y: -i,
+          rows: i,
+          mesh: this.mesh,
+          duplicateMesh: this.duplicateMesh,
+          offset:
+            i / this.parameters.activeRows +
+            Math.random() / this.parameters.activeRows,
+        });
+      }
+    }
   }
 
   createLight() {
-    this.pointLight = new THREE.PointLight(0xff0000, 10, 100);
-    this.pointLight.position.set(2, 2, -2);
+    this.pointLight = new THREE.PointLight(0xffffff, 10, 100);
+    this.pointLight.position.set(0, 0, 6);
 
-    this.directionalLight = new THREE.DirectionalLight(0xffffff, 0.8);
-    this.directionalLight.position.set(1, 1, 3);
+    this.ambientLight = new THREE.AmbientLight(0xffffff, 1);
 
-    this.scene.add(this.pointLight, this.directionalLight);
+    this.directionalLight = new THREE.DirectionalLight(0xffffff, 1);
+    this.directionalLight.position.set(4, 2, 3);
+
+    this.scene.add(this.pointLight, this.ambientLight, this.directionalLight);
   }
 
   createControls() {
@@ -79,6 +159,37 @@ export default class Experience {
 
   render() {
     this.renderer.render(this.scene, this.camera);
+
+    this.parameters.time += this.parameters.step;
+
+    this.parameters.playhead = this.parameters.time % 1;
+
+    this.group.position.setY(
+      -this.parameters.playhead * this.parameters.activeRows
+    );
+
+    this.animation.forEach((animation) => {
+      if (animation.rows < this.parameters.activeRows) {
+        let theta = this.parameters.playhead + animation.offset;
+
+        if (theta > 1) {
+          theta = (theta - 1) ** 3;
+
+          animation.mesh.position.setY(
+            animation.y +
+              (1 - theta) * 10 +
+              this.parameters.playhead * this.parameters.activeRows
+          );
+          animation.duplicateMesh.visible = true;
+        } else {
+          theta = theta ** 3;
+          animation.mesh.position.setY(animation.y + (1 - theta) * 10);
+          animation.duplicateMesh.visible = false;
+        }
+      } else {
+        animation.duplicateMesh.visible = false;
+      }
+    });
   }
 
   update() {
